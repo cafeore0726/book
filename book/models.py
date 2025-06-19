07f736a6c40e83c291c8bb587f30493dccf232f5
reo0726 <kafeore0726ri@docomo.ne.jp>
2025-06-19 10:55:15 +0900

    モデルの作成

--- book/models.py
+++ book/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+# モデルの定義
+class Expence(models.Model):
+    name = models.CharField(max_length=100) # 項目名
+    amount = models.DecimalField(max_digits=10, decimal_places=2)   # 金額
+    recorded_at = models.DateTimeField(auto_now_add=True)   # 記録日時
