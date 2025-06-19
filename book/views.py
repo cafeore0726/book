f76b2561a48038b8418646f5ae747408714fa05d
reo0726 <kafeore0726ri@docomo.ne.jp>
2025-06-19 11:47:22 +0900

    Expenseモデルのrecorded_atフィールドが存在しない問題を修正

--- book/views.py
+++ book/views.py
@@ -2,18 +2,15 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.db.models import Sum
+from .models import Expense
+from .serializers import ExpenseSerializer
 
-from .models import Expence
-from .serializers import ExpenceSerializer
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all().order_by('-date')
+    serializer_class = ExpenseSerializer
 
-# Create your views here.
-
-class ExpenceViewSet(viewsets.ModelViewSet):
-    queryset = Expence.objects.all()  # 全てのExpenceオブジェクトを取得
-    serializer_class = ExpenceSerializer # シリアライザを指定
-    
-    @api_view(['GET']) # 合計金額を取得するエンドポイント
-    def total_expence(request):
-        total = Expence.objects.aggregate(Sum('amount'))['amount__sum'] or 0
-        status = "黒字" if total >= 0 else "赤字" # 状態を判定
-        return Response({"total":total, "status": status})  # 合計金額と状態を返す
+@api_view(['GET'])
+def budget_status(request):
+    total = Expense.objects.aggregate(Sum('amount'))['amount__sum'] or 0
+    status = "黒字" if total >= 0 else "赤字"
+    return Response({"total": total, "status": status})
